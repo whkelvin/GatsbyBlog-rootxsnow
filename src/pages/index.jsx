a5db7d2526057aa8c9afccfabf73e89b4626d339
whkelvin <whkelvin@icloud.com>
2021-01-10 19:24:56 -0400

    test different query

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,22 +7,17 @@ export default function IndexPage() {
 
     const data = useStaticQuery(
     graphql`
-      query {
-        allMarkdownRemark(
-          filter: { id: { eq: "80fedd98-bad7-5dee-9ba8-73ba29e87fb7" } }
-        ) {
-          nodes {
-            html
-          }
-        }
+    query {
+      markdownRemark {
+        html
       }
+    }
     `
     );
 
-    var mdContent = data.allMarkdownRemark.nodes[0].html;
 
     return  <div className={contentStyle.mdContent}
-                  dangerouslySetInnerHTML={{__html: mdContent}}>
+                  dangerouslySetInnerHTML={{__html: data.markdownRemark.html}}>
             </div>;
 
 }
