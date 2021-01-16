77bb7f777230f0ce417a09c62257db66f62bc557
whkelvin <whkelvin@icloud.com>
2021-01-16 18:40:50 -0400

    changed sidebar to functional component

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,29 +1,72 @@
 import React from "react";
+import { StaticQuery, graphql, Link } from "gatsby";
+
 var sidebarStyle = require("./sidebar.module.scss");
 
-class Sidebar extends React.Component {
-  render() {
+export default function Sidebar(props) {
     return (
-      <div
-        className={[
-          sidebarStyle.sidebar,
-          this.props.open ? sidebarStyle.opened : sidebarStyle.closed,
-        ].join(" ")}
-      >
-          <div className={[sidebarStyle.sidebarContent, 
-                            this.props.open ? sidebarStyle.showSidebarContent : sidebarStyle.hideSidebarContent].join(" ")}>
-            施工中
-          </div>
-      </div>
+        <StaticQuery
+            query={graphql`
+                query MyQuery {
+                    site(siteMetadata: { menuLinks: {}, title: {} }) {
+                        siteMetadata {
+                            menuLinks {
+                                link
+                                name
+                            }
+                        }
+                    }
+                }
+            `}
+            render={(data) => (
+                <div
+                    className={[
+                        sidebarStyle.sidebar,
+                        props.open ? sidebarStyle.opened : sidebarStyle.closed,
+                    ].join(" ")}
+                >
+                    <div
+                        className={[
+                            sidebarStyle.sidebarContent,
+                            props.open
+                                ? sidebarStyle.showSidebarContent
+                                : sidebarStyle.hideSidebarContent,
+                        ].join(" ")}
+                    >
+                        <ul
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                flex: 1,
+                            }}
+                        >
+                            {data.site.siteMetadata.menuLinks.map((link) => (
+                                <li
+                                    key={link.name}
+                                    style={{
+                                        listStyleType: `none`,
+                                        padding: `1rem`,
+                                    }}
+                                >
+                                    <Link
+                                        style={{ color: `white` }}
+                                        to={link.link}
+                                    >
+                                        {link.name}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            )}
+        />
     );
-  }
-  //openSidebar() {
-  //  this.setState({ open: true });
-  //}
+    //openSidebar() {
+    //  this.setState({ open: true });
+    //}
 
-  //closeSidebar() {
-  //  this.setState({ open: false });
-  //}
+    //closeSidebar() {
+    //  this.setState({ open: false });
+    //}
 }
-
-export default Sidebar;
