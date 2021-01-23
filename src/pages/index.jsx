c3811b7b7dd8a8608ba8c6b3a632e8032f0ad317
whkelvin <whkelvin@icloud.com>
2021-01-23 16:35:26 -0400

    removed home content

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,37 +5,23 @@ import Content from "../components/content";
 var contentStyle = require("../components/content.module.scss");
 
 export default function IndexPage({ data }) {
-    return (
-        <Content>
-            <div className={contentStyle.mdContent}>
-                {data.allMarkdownRemark.edges.map(({ node }) => (
-                    <div key={node.id}>
-                        <h3>
-                            {node.frontmatter.title}{" "}
-                            <span>— {node.frontmatter.date}</span>
-                        </h3>
-                        <p>{node.excerpt}</p>
-                    </div>
-                ))}
-            </div>
-        </Content>
-    );
+  return <Content>首頁施工中....</Content>;
 }
 
 export const query = graphql`
-    query {
-        allMarkdownRemark {
-            totalCount
-            edges {
-                node {
-                    id
-                    frontmatter {
-                        title
-                        date(formatString: "DD MMMM, YYYY")
-                    }
-                    excerpt
-                }
-            }
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
         }
+      }
     }
+  }
 `;
